6871611384f63e41e1302b37bd0e585a2b75df9f
NickTheRussian <nicholas.choate1@outlook.com>
2017-02-20 23:04:21 -0600

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Group.Startup))]
+namespace Group
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
